5425deddbd78e331b0a6cecb4f0a1f7395e5efc2
ivsoca <socanter23@gmail.com>
2023-11-14 12:17:27 -0300

    Agrregue imagenes y arregle un par de cosas

--- preguntasSemana16.js
+++ preguntasSemana16.js
@@ -8,8 +8,8 @@ document.forms["quizForm"].addEventListener("submit", function (event) {
         "true", //3
         "false", //4
         "true", //5
-        "false", //6
-        "false", //7
+        "true", //6
+        "true", //7
         "true", //8
         "true", //9
         "true"]; //10
